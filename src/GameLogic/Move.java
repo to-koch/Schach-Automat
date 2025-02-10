865279d69d0deb2d109a940cea1d9d81c83de157
to-koch <tmk05@web.de>
2025-02-10 16:56:32 +0100

    Commit 10.02.2025

---
+++ src/GameLogic/Move.java
@@ -0,0 +1,20 @@
+/**
+ * Container class for a move
+ */
+
+package GameLogic;
+
+public class Move {
+
+    /**
+     * coordinates on the board of source and destination tile
+     */
+    public int x_dest, y_dest, x_src, y_src;
+
+    public Move(int x_dest, int y_dest, int x_src, int y_src) {
+        this.x_dest = x_dest;
+        this.y_dest = y_dest;
+        this.x_src = x_src;
+        this.y_src = y_src;
+    }
+}
--- src/GameLogic/MoveTools.java
+++ src/GameLogic/MoveTools.java
@@ -1,4 +1,47 @@
+/**
+ * Methods to get and evaluate moves
+ */
+
 package GameLogic;
 
-public class MoveTools {
+import java.util.ArrayList;
+
+public abstract class MoveTools {
+
+    /**
+     * finds all moves that can be executed by a figure
+     * @param board the board on which to determine the moves
+     * @param x the x-coordinate of the figure
+     * @param y the y-coordinate of the figure
+     * @param color the color of the currently active player
+     * @return list of possible moves
+     */
+    public static ArrayList<Move> get_moves(BoardConfig board, int x, int y, Figure.Color color) {
+        int index = y * 8 + x;
+        Figure.Color fig_color = Figure.get_color(board.board[index]);
+        Figure.Type fig_type = Figure.get_type(board.board[index]);
+        // return if tile does not contain a valid figure or a figure of different color
+        if (fig_type == null || fig_color == null || fig_color != color) {
+            return null;
+        }
+
+        ArrayList<Move> moves =  new ArrayList<>();
+        if (fig_type == Figure.Type.BISHOP) {
+
+        } else if (fig_type == Figure.Type.KING) {
+
+        } else if (fig_type == Figure.Type.KNIGHT) {
+
+        } else if (fig_type == Figure.Type.PAWN) {
+
+        } else if (fig_type == Figure.Type.QUEEN) {
+
+        } else if (fig_type == Figure.Type.ROOK) {
+
+        }
+
+        return moves;
+    }
+
+
 }
