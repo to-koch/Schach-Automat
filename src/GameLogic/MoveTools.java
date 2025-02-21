d98d884d463de03f47a9b64237f750bb04d42e4c
to-koch <tmk05@web.de>
2025-02-21 16:55:01 +0100

    Commit 21.02.2025

--- src/GameLogic/MoveTools.java
+++ src/GameLogic/MoveTools.java
@@ -1,93 +1,331 @@
 /**
- * Methods to get and evaluate moves
+ * Methods to get possible moves for a figure
  */
 
 package GameLogic;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public abstract class MoveTools {
 
     /**
-     * finds all moves that can be executed by a figure
+     * finds all moves that can be executed by a figure (ensures that path is clear as well)
      * @param board the board on which to determine the moves
      * @param x the x-coordinate of the figure
      * @param y the y-coordinate of the figure
      * @param color the color of the currently active player
-     * @return array of possible moves
+     * @return list of possible moves
      */
-    public static Move[] get_moves(BoardConfig board, int x, int y, Figure.Color color) {
+    public static ArrayList<Move> get_moves(BoardConfig board, int x, int y, Figure.Color color) {
+        if (x > 7 || x < 0 || y > 7 || y < 0) {
+            return null;
+        }
         int index = y * 8 + x;
         Figure.Color fig_color = Figure.get_color(board.board[index]);
         Figure.Type fig_type = Figure.get_type(board.board[index]);
-        // return if tile does not contain a valid figure or a figure of different color
+        boolean already_moved = Figure.get_already_moved(board.board[index]);
+        // aboard if tile does not contain a valid figure or a figure of different color
         if (fig_type == null || fig_color == null || fig_color != color) {
             return null;
         }
 
-        Move[] moves = get_all_moves(x, y, fig_type, fig_color);
-        // filter illegal moves
-        int count = 0;
-        Move[] tmp = new Move[moves.length];
-        for (Move m : moves) {
-            if (m.y_dest < 0 || m.y_dest > 7 || m.x_dest < 0 || m.x_dest > 7) {
-                continue;
-            }
-            tmp[count] = m;
-            count++;
-        }
-
-        return Arrays.copyOf(tmp, count);
-    }
-
-    /**
-     * helper method that finds all moves that could potentially be possible
-     * @param x the x-coordinate of the figure
-     * @param y the y-coordinate of the figure
-     * @param fig_type the type of the figure
-     * @param color the color of the figure
-     * @return array of potentially possible moves
-     */
-    private static Move[] get_all_moves(int x, int y, Figure.Type fig_type, Figure.Color color) {
-        Move[] moves = null;
+        ArrayList<Move> moves = new ArrayList<>();
         if (fig_type == Figure.Type.BISHOP) {
-            moves = new Move[28];
             for (int i = 1; i < 8; i++) {
-                moves[i] = new Move(x, y, x + i, y + i);
+                int x_new = x + i;
+                int y_new = y + i;
+                if (x_new > 7 || y_new > 7) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                    break;
+                } else {
+                    break;
+                }
             }
             for (int i = 1; i < 8; i++) {
-                moves[7 + i] = new Move(x, y, x + i, y - i);
+                int x_new = x + i;
+                int y_new = y - i;
+                if (x_new > 7 || y_new < 0) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                    break;
+                } else {
+                    break;
+                }
             }
             for (int i = 1; i < 8; i++) {
-                moves[14 + i] = new Move(x, y, x - i, y + i);
+                int x_new = x - i;
+                int y_new = y + i;
+                if (x_new < 0 || y_new > 7) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                    break;
+                } else {
+                    break;
+                }
             }
             for (int i = 1; i < 8; i++) {
-                moves[21 + i] = new Move(x, y, x - i, y - i);
+                int x_new = x - i;
+                int y_new = y - i;
+                if (x_new < 0 || y_new < 0) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                    break;
+                } else {
+                    break;
+                }
             }
         } else if (fig_type == Figure.Type.KING) {
-            moves = new Move[8];
-            for (int i = 0; i < 3; i++) {
-                moves[2 * i] = new Move(x, y, x - 1 + i, y - 1);
-                moves[2 * i + 1] = new Move(x, y, x - 1 + i, y + 1);
+            int[] x_new = {x-1, x, x+1, x-1, x+1, x-1, x, x+1};
+            int[] y_new = {y-1, y-1, y-1, y, y, y+1, y+1, y+1};
+            for (int i = 0; i < 8; i++) {
+                if (x_new[i] > 7 || x_new[i] < 0 || y_new[i] > 7 || y_new[i] < 0) {
+                    continue;
+                }
+                char dest_fig = board.board[y_new[i] * 8 + x_new[i]];
+                if (dest_fig == 0 || Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new[i], y_new[i]));
+                }
             }
-            moves[6] = new Move(x, y, x - 1, y);
-            moves[7] = new Move(x, y, x + 1, y);
         } else if (fig_type == Figure.Type.KNIGHT) {
-            moves = new Move[8];
-            moves[0] = new Move(x, y, x - 1, y - 2);
-            moves[1] = new Move(x, y, x + 1, y - 2);
-            moves[2] = new Move(x, y, x - 1, y + 2);
-            moves[3] = new Move(x, y, x + 1, y + 2);
-            moves[4] = new Move(x, y, x - 2, y - 1);
-            moves[5] = new Move(x, y, x - 2, y + 1);
-            moves[6] = new Move(x, y, x + 2, y - 1);
-            moves[7] = new Move(x, y, x + 2, y + 1);
+            int[] x_new = {x-1, x+1, x-1, x+1, x-2, x-2, x+2, x-2};
+            int[] y_new = {y-2, y-2, y+2, y+2, y-1, y+1, y-1, y+1};
+            for (int i = 0; i < 8; i++) {
+                if (x_new[i] > 7 || x_new[i] < 0 || y_new[i] > 7 || y_new[i] < 0) {
+                    continue;
+                }
+                char dest_fig = board.board[y_new[i] * 8 + x_new[i]];
+                if (dest_fig == 0 || Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new[i], y_new[i]));
+                }
+            }
         } else if (fig_type == Figure.Type.PAWN) {
-
+            int delta_y = (fig_color == Figure.Color.WHITE) ? -1 : 1;
+            int upper = already_moved ? 3 : 2;
+            for (int i = 1; i < upper; i++) {
+                int y_new = y + i * delta_y;
+                if (y_new > 7 || y_new < 0 || board.board[y_new * 8 + x] != 0) {
+                    break;
+                }
+                moves.add(new Move(x, y, x, y_new));
+            }
+            for (int i = -1; i < 2; i += 2) {
+                int y_new = y + delta_y;
+                int x_new = x + i;
+                if (y_new > 7 || y_new < 0 || x_new > 7 || x_new < 0) {
+                    continue;
+                }
+                char dest_fig = board.board[y_new * 8 + x_new];
+                if (dest_fig != 0 && Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                }
+            }
         } else if (fig_type == Figure.Type.QUEEN) {
-
+            for (int i = 1; i < 8; i++) {
+                int x_new = x + i;
+                int y_new = y + i;
+                if (x_new > 7 || y_new > 7) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                    break;
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i < 8; i++) {
+                int x_new = x + i;
+                int y_new = y - i;
+                if (x_new > 7 || y_new < 0) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                    break;
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i < 8; i++) {
+                int x_new = x - i;
+                int y_new = y + i;
+                if (x_new < 0 || y_new > 7) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                    break;
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i < 8; i++) {
+                int x_new = x - i;
+                int y_new = y - i;
+                if (x_new < 0 || y_new < 0) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y_new));
+                    break;
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i < 8; i++) {
+                int x_new = x + i;
+                if (x_new > 7) {
+                    break;
+                }
+                char dest_fig = board.board[y * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y));
+                    break;
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i < 8; i++) {
+                int x_new = x - i;
+                if (x_new < 0) {
+                    break;
+                }
+                char dest_fig = board.board[y * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y));
+                    break;
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i < 8; i++) {
+                int y_new = y + i;
+                if (y_new > 7) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x, y_new));
+                    break;
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i < 8; i++) {
+                int y_new = y - i;
+                if (y_new < 0) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x, y_new));
+                    break;
+                } else {
+                    break;
+                }
+            }
         } else if (fig_type == Figure.Type.ROOK) {
-
+            for (int i = 1; i < 8; i++) {
+                int x_new = x + i;
+                if (x_new > 7) {
+                    break;
+                }
+                char dest_fig = board.board[y * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y));
+                    break;
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i < 8; i++) {
+                int x_new = x - i;
+                if (x_new < 0) {
+                    break;
+                }
+                char dest_fig = board.board[y * 8 + x_new];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x_new, y));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x_new, y));
+                    break;
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i < 8; i++) {
+                int y_new = y + i;
+                if (y_new > 7) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x, y_new));
+                    break;
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i < 8; i++) {
+                int y_new = y - i;
+                if (y_new < 0) {
+                    break;
+                }
+                char dest_fig = board.board[y_new * 8 + x];
+                if (dest_fig == 0) {
+                    moves.add(new Move(x, y, x, y_new));
+                } else if (Figure.get_color(dest_fig) != color) {
+                    moves.add(new Move(x, y, x, y_new));
+                    break;
+                } else {
+                    break;
+                }
+            }
         }
         return moves;
     }
